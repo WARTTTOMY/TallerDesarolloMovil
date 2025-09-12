973023ffded66714c322d9612bdd9361ebec64b7
juanpjs99 <juanp.js1999@gmail.com>
2025-09-12 17:31:13 -0500

    Actualicé navegaor en app.js, cree Editar_tarea.js y empecé a realizar los métodos de modificar tarea

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { PaperProvider } from "react-native-paper"; 
-import CreateProfile from "./src/Screens/CreateProfile";
+import AddTaskScreen from "./src/Screens/";
 import ViewProfile from "./src/Screens/ViewProfile";
 
 const Stack = createNativeStackNavigator();
@@ -13,14 +13,19 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator initialRouteName="CreateProfile">
           <Stack.Screen 
-            name="CreateProfile" 
-            component={CreateProfile} 
-            options={{ title: 'Crear Perfil' }}
+            name="Editar_Tarea" 
+            component={EditarTarea} 
+            options={{ title: 'Editar Tarea' }}
           />
           <Stack.Screen 
-            name="ViewProfile" 
+            name="Crear_tarea" 
             component={ViewProfile} 
-            options={{ title: 'Ver Perfil' }}
+            options={{ title: 'Crear tarea' }}
+          />
+          <Stack.Screen 
+            name="Listar_tareas" 
+            component={ViewProfile} 
+            options={{ title: 'Ver Lista de tareas' }}
           />
         </Stack.Navigator>
       </NavigationContainer>
