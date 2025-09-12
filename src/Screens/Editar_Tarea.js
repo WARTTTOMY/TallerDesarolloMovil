84da3cc661fa83b77d1f2649b76b85ced8a0238a
juanpjs99 <juanp.js1999@gmail.com>
2025-09-12 18:23:31 -0500

    Se creo el método de validación de tareas

--- src/Screens/Editar_Tarea.js
+++ src/Screens/Editar_Tarea.js
@@ -1,13 +1,41 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, Button, Alert, ScrollView, Image } from "react-native";
-import { Card, TextInput } from "react-native-paper";
+import { View, Text, TextInput, Button, Alert, StyleSheet } from "react-native";
+
 import styles from "../Styles/styles"; // Importación de los estilos
+import { ScrollView } from "react-native/types_generated/index";
 
-const editarPerfil = ({navigation}) => {
 
-    //HOOKS PARA EDITAR TAREAS
-    const [titulo, setTitulo] = useState ("");
-    const [descripcion, setDescripcion] = useState ("");
-    const [fecha, setFecha] = useState ("");
-    const [prioridad, setPrioridad] = useState ("");
-}
+const EditarTarea = ({ tarea, onActualizar, navigation }) => {
+  const [titulo, setTitulo] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [prioridad, setPrioridad] = useState("");
+
+  // useEffect: carga inicial de datos en los inputs
+  useEffect(() => {
+    if (tarea) {
+      setTitulo(tarea.titulo);
+      setDescripcion(tarea.descripcion);
+      setFecha(tarea.fecha);
+      setPrioridad(tarea.prioridad);
+    }
+  }, [tarea]);
+
+  //actualizacion
+
+  const handleAcutalizar = () => {
+    if (!titulo.trim()) {
+        Alert.alert (
+            "error titulo",
+            "el título es obligatorio"
+        );
+        return;
+    }
+    //Se llama la función y se actualiza la tarea 
+    onActualizar ({ 
+        ...tarea, titulo, descripcion, fecha, prioridad
+    });
+    navigation.goBack(); //volver 
+  };
+
+  
