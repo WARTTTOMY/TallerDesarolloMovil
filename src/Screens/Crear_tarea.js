1ca1f1e1f15c00978776a509556805456fe2f8df
Arconte27 <luismiescobarm@gmail.com>
2025-09-12 18:11:49 -0500

    cree la vista de crear tarea en el repositorio de crear_tarea

---
+++ src/Screens/Crear_tarea.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, Button, Alert, ScrollView } from "react-native";
+import { TextInput, Card } from "react-native-paper";
+import styles from "../Styles/styles"; // importa tu hoja de estilos
+
+const CrearTareaScreen = ({ navigation }) => {
+  // Hooks para los campos
+  const [titulo, setTitulo] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [fecha, setFecha] = useState("");
+  const [prioridad, setPrioridad] = useState("");
+  
+
+}
