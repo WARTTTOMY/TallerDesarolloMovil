b911393c494a9598b704f2365e48c8cbba03e74b
Arconte27 <luismiescobarm@gmail.com>
2025-09-12 18:23:02 -0500

    Guardamos la tarea y validamos los cambio

--- src/Screens/Crear_tarea.js
+++ src/Screens/Crear_tarea.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from "react";
+
+  
+  import React, { useState, useEffect } from "react";
 import { View, Text, Button, Alert, ScrollView } from "react-native";
 import { TextInput, Card } from "react-native-paper";
 import styles from "../Styles/styles"; // importa tu hoja de estilos
@@ -10,5 +12,29 @@ const CrearTareaScreen = ({ navigation }) => {
   const [fecha, setFecha] = useState("");
   const [prioridad, setPrioridad] = useState("");
   
+
+  // validamos cada vez que chagamos un cambio 
+  useEffect(() => {
+    const Validar =
+      titulo.trim().length > 0 &&
+      descripcion.trim().length > 0 &&
+      fecha.trim().length > 0 &&
+      prioridad.trim().length > 0;
+
+    setIsFormValid(Validar);
+  }, [titulo, descripcion, fecha, prioridad]);
+
+  // Guardar la tarea
+  const Guardar = () => {
+    if (!FormValidar) {
+      Alert.alert("Error", "Por favor, complete todos los campos.");
+      return;
+    }
+
+    const nuevaTarea = { titulo, descripcion, fecha, prioridad };
+
+    // Navegar a otra pantalla pasando la tarea creada
+    navigation.navigate("ListaTareas", { tarea: nuevaTarea });
+  };
 
 }
