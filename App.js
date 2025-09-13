12a6c2384119b42c4868aec57f0fabfd7f018416
WARTTTOMY <warttomy@gmail.com>
2025-09-12 21:19:08 -0500

    Se guardan cambios en navegacion y se elimina gesture handler

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { View, Text } from 'react-native';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { Button } from "react-native";
@@ -9,36 +13,28 @@ import CrearTareaScreen from "./src/Screens/Crear_tarea";
 import EditarTareaScreen from "./src/Screens/Editar_Tarea"; 
 import ListaTareasScreen from "./src/Screens/ListaTareas"; 
 
-const Stack = createStackNavigator();
+
+
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: CrearTareaScreen,
+  },
+});
+
+
+const Navigation = createStaticNavigation(RootStack);
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="CrearTarea" component={CrearTareaScreen} />
-        <Stack.Screen name="EditarTarea" component={EditarTareaScreen} />
-        <Stack.Screen name="ListarTareas" component={ListaTareasScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <Navigation>
+     <Navigation.Navigator initialRouteName="Home">
+        <Navigation.Screen name="Home" component={HomeScreen} />
+        <Navigation.Screen name="CrearTarea" component={CrearTareaScreen} />
+        <Navigation.Screen name="EditarTarea" component={EditarTareaScreen} />
+        <Navigation.Screen name="ListarTareas" component={ListaTareasScreen} />
+      </Navigation.Navigator>
+  </Navigation>
 }
 
-function HomeScreen({ navigation }) {
-  return (
-    <>
-      <Button
-        title="Crear Tarea"
-        onPress={() => navigation.navigate("CrearTarea")}
-      />
-      <Button
-        title="Editar Tarea"
-        onPress={() => navigation.navigate("EditarTarea")}
-      />
-      <Button
-        title="Listar Tareas"
-        onPress={() => navigation.navigate("ListarTareas")}
-      />
-    </>
-  );
-}
+
+
+
